ba0502a1b6eaca668259467c47d1f710b43e36e1
Eyal-2022 <1002135036@edu-darom.org.il>
2026-03-11 19:08:58 +0200

    Update registers.vhd

--- registers.vhd
+++ registers.vhd
@@ -1,3 +1,4 @@
+-- תרגיל 1
 -- תכננו דלגלג  סינכרוני עם כניסה, כניסת שעון ויציאה
 -- כאשר יש חזית עולה של השעון, הערך של הכניסה ישמר ביציאה.בכל זמן אחר היציאה לא משתנה
 library IEEE;
@@ -20,4 +21,29 @@ begin
 	   end if;
 	end process;
 end Behave;
+-- תרגיל 2
+-- כתבו תוכנית לדלגלג סינכרוניעם כניסה, כניסת שעון, כניסת איפוס אסיכרוני שמאפסת את הכניסה לאפס ויציאה
+-- Cyclone v 5CEBA4F23C7
+--  pin planner: clk->PIN_M9 , reset-> PIN_U13 , D-> PIN_U7 , Q-> PIN_AA2
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+entity tar3 is
+    Port ( clk: in std_logic;
+	        reset: in std_logic;
+	        d:   in std_logic;
+			  q:   out std_logic);
+end tar3;
+
+architecture Behave of tar3 is
+begin
+	process(clk,reset)
+	begin
+		if reset='1' 
+			then q<='0';
+		elsif rising_edge(clk) 
+			then q<=d;
+	   end if;
+	end process;
+end Behave;
+		   
