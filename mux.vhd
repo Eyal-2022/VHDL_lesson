c3b5886fab94178a213b671b51473deb7d51a15c
Eyal-2022 <1002135036@edu-darom.org.il>
2026-03-11 22:54:17 +0200

    Create mux.vhd

---
+++ mux.vhd
@@ -0,0 +1,16 @@
+-- כתבו תוכנית למרבב 2 לאחד
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tar3 is
+    port (
+        A   : in  std_logic; 
+        B   : in  std_logic; 
+		  sel : in std_logic;
+        y   : out std_logic);
+end tar3;
+
+architecture behave of tar3 is
+begin
+  	y<= (a and not(sel)) or (b and sel);
+end behave;
