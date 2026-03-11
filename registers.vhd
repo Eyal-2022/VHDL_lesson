9a001efd4381c146e6aed350ae4cb41026fff555
Eyal-2022 <1002135036@edu-darom.org.il>
2026-03-11 19:33:55 +0200

    Rename entity and architecture from tar3 to tar2

--- registers.vhd
+++ registers.vhd
@@ -28,14 +28,14 @@ end Behave;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity tar3 is
+entity tar2 is
     Port ( clk: in std_logic;
 	        reset: in std_logic;
 	        d:   in std_logic;
 			  q:   out std_logic);
-end tar3;
+end tar2;
 
-architecture Behave of tar3 is
+architecture Behave of tar2 is
 begin
 	process(clk,reset)
 	begin
