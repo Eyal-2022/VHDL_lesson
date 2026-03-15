70a70d85386d0848867054a2a44ff8c3306a6ea5
Eyal-2022 <1002135036@edu-darom.org.il>
2026-03-15 17:32:31 +0200

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -1,16 +1,19 @@
--- כתבו תוכנית למרבב 2 לאחד באמצעות שערים לוגים בלבד
+-- תרגיל 1
+-- (mux)כתבו תוכנית למרבב 2 ל-1 באמצעות שערים לוגים בלבד
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity tar3 is
+entity tar1 is
     port (
         A   : in  std_logic; 
         B   : in  std_logic; 
 		  sel : in std_logic;
         y   : out std_logic);
-end tar3;
+end tar1;
 
-architecture behave of tar3 is
+architecture behave of tar1 is
 begin
   	y<= (a and not(sel)) or (b and sel);
 end behave;
+-- תרגיל 2
+-- כתבו תוכנית למפלג 1 ל-2(demux)
