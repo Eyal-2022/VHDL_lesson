0f3aeaa301845adf275d92ace0843e9df2bcd9ba
Eyal-2022 <1002135036@edu-darom.org.il>
2026-03-16 11:21:22 +0200

    Implement 4-to-2 priority encoder in VHDL
    Added a priority encoder that prioritizes the lowest active input.

--- Encoder-Decoder.vhd
+++ Encoder-Decoder.vhd
@@ -42,6 +42,35 @@ begin
 end logic;
 
 -- תרגיל 3
--- כתבו מקודד עדיפות 4 ל-2(Priority Encoder)
--- מקודד רגיל יפעל באופן תקין כאשר יש כניסה אחת בלבד פעילה עם מידע, אך מה קורה כאשר כמה כניסות פעילות עם מידע?
+--  כתבו מקודד עדיפות 4 ל-2 לכניסה הנמוכה ביותר( Priority Encoder)
+--כלומר לדוגמא אם כניסה די(0) שווה ל-1 וגם די(1) שווה ל-1 אז תינתן עדיפות לדי(0)
+-- מקודד רגיל יפעל באופן תקין כאשר יש כניסה אחת בלבד פעילה עם מידע, אך מה קורה כאשר כמה כניסות פעילות עם מידע
 -- לצורך כך יש את המקודד עדיפות שהוא הרחבה של מקודד רגיל, באמצעתו ניתן עדיפות לכניסה שנרצה 
+-- pin planner: DO->U13, D1->V13, D2->T13, D3->T12, Y0->AA1, Y1->W2.
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity tar3 is
+    port(
+        D0 : in std_logic;
+        D1 : in std_logic;
+        D2 : in std_logic;
+        D3 : in std_logic;
+        Y  : out std_logic_vector(1 downto 0));
+end tar3;
+architecture behave of tar3 is
+begin
+	process(D0, D1, D2, D3)
+	begin
+		 if D0 = '1' then
+			  Y <= "00";
+		 elsif D1 = '1' then
+			  Y <= "01";
+		 elsif D2 = '1' then
+			  Y <= "10";
+		 elsif D3 = '1' then
+			  Y <= "11";
+		 else
+			  Y <= "00";
+		 end if;
+	end process;
+end behave;
